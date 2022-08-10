acec5d05232d1d9f3ea783c516e4c6e11e0e282a
Moises Azevedo <moisesdev2022@gmail.com>
2022-08-10 09:22:36 -0300

    add: funcionalidades js; projeto concluído

--- Js/script.js
+++ Js/script.js
@@ -58,7 +58,7 @@ function desenhaTabela(){
     <div id="total">
         <p></p>
         <p id="tot-tablet">Total</p>  
-        <p>R$${total}</p> 
+        <p>${formater.format(total)}</p> 
     </div>`   
     
     if (extrato1 == []){
@@ -176,14 +176,93 @@ function extrato_function(c) {
    /*   =========================================================
                    LINHA DO TOTAL
      ========================================================= */
-    document.getElementById('total').remove()               
+
+    document.getElementById('total').remove()             
     document.querySelector('.extrato').innerHTML += `
     <div id="total">
         <p></p>
         <p id="tot-tablet">Total</p>  
-        <p>R$${total}</p> 
-    </div>   `
+        <p>${formater.format(total)}</p> 
+    </div>`
 }
+/*=========================================================
+                Victor
+========================================================= */
+
+//Formata o total para tipo de moeda
+var formater = new Intl.NumberFormat('pt-BR' , {
+    style: 'currency',
+    currency: 'BRL' , 
+    maximumFractionDigits: 2,
+})
+
+function MascaraMoeda(objTextBox, SeparadorMilesimo, SeparadorDecimal, e) {
+    var sep = 0;
+    var key = "";
+    var i = (j = 0);
+    var len = (len2 = 0);
+    var strCheck = "0123456789";
+    var aux = (aux2 = "");
+    var whichCode = window.Event ? e.which : e.keyCode;
+    if (whichCode == 13 || whichCode == 8) return true;
+    key = String.fromCharCode(whichCode); // Valor para o código da Chave
+    if (strCheck.indexOf(key) == -1) return false; // Chave inválida
+    len = objTextBox.value.length;
+    for (i = 0; i < len; i++)
+      if (
+        objTextBox.value.charAt(i) != "0" &&
+        objTextBox.value.charAt(i) != SeparadorDecimal
+      )
+        break;
+    aux = "";
+    for (; i < len; i++)
+      if (strCheck.indexOf(objTextBox.value.charAt(i)) != -1)
+        aux += objTextBox.value.charAt(i);
+    aux += key;
+    len = aux.length;
+    if (len == 0) objTextBox.value = "";
+    if (len == 1) objTextBox.value = "0" + SeparadorDecimal + "0" + aux;
+    if (len == 2) objTextBox.value = "0" + SeparadorDecimal + aux;
+    if (len > 2) {
+      aux2 = "";
+      for (j = 0, i = len - 3; i >= 0; i--) {
+        if (j == 3) {
+          aux2 += SeparadorMilesimo;
+          j = 0;
+        }
+        aux2 += aux.charAt(i);
+        j++;
+      }
+      objTextBox.value = "";
+      len2 = aux2.length;
+      for (i = len2 - 1; i >= 0; i--) objTextBox.value += aux2.charAt(i);
+      objTextBox.value += SeparadorDecimal + aux.substr(len - 2, len);
+    }
+    return false;
+  }
+
+
+
+/*=========================================================
+                
+========================================================= */
+/*=========================================================
+                Testar Campo valor
+========================================================= */
+
+/* function testaCampoValor(e){
+    if (e.target.value.length == 0){
+        e.target.value = 'R'
+    }
+
+    if (e.target.value.length == 1){
+        e.target.value = '$'
+    }
+
+    if ((/[0-9 -R$]/g).test(e.key)){
+        e.target.value += e.key
+    }
+} */
 
 
 /* =============================================================================
@@ -211,46 +290,20 @@ function limpaDados(p){
 
 desenhaTabela()
 
-/* var extrato = localStorage.getItem('extrato')    //pega do localStorage como string
-    if (extrato != null) {
-        var extrato1 = JSON.parse(extrato)              //pega a variavel extrato e converte em objeto
-    } else {
-        var extrato1 = []                               //Caso não exista nada no localStorage, extrato1 será um array vazio
-    }
-    console.log(extrato1)
-    
-    //pegar o total do local storage
-    var total0 =  localStorage.getItem('total')
-    var total =  JSON.parse(total0)
-    if (total == null) {
-        var total = 0
-    }
 
-    //printa na tela
-    for (imprime in extrato1) {
-        if (extrato1[imprime].p1 == "venda"){
-            document.querySelector('#ext-itens').innerHTML += `
-            <div id="merca">
-                <p>+</p>
-                <p>${extrato1[imprime].name}</p> 
-                <p >R$${extrato1[imprime].valor}</p>    
-            </div>`
-        } else{
-            
-            document.querySelector('#ext-itens').innerHTML += `
-            <div id="merca">
-                <p>-</p>
-                <p>${extrato1[imprime].name}</p> 
-                <p >R$${extrato1[imprime].valor}</p>    
-            </div>`
-        };                                     
-    };
+/*=========================================================
+                menuDelete (responsividade)
+========================================================= */
 
-    //printa na tela o total
-    document.getElementById('total').remove()               
-    document.querySelector('.extrato').innerHTML += `
-    <div id="total">
-        <p></p>
-        <p id="tot-tablet">Total</p>  
-        <p>R$${total}</p> 
-    </div>`    */
+function menuDelete(){
+    document.querySelector('body').innerHTML += 
+    `<div class='menuLimpa'>
+        <h3 onclick='end_menuDelete()'>✖</h3>
+        <p onclick="limpaDados(0)" >Limpar dados</p>
+        <p>Cadastro de transações</p>
+    </div>`
+}
+
+function end_menuDelete() {
+    document.querySelector('.menuLimpa').remove()
+}
